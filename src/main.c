4db60e4075310e7bce1ffe1c8ca7efce7d77ed63
rexdez <desbeta@zemuria.com>
2025-01-12 09:57:51 +0530

    fix: Adding libwebp headers

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#include "webp/encode.h"
+#include "webp/decode.h"
+
+
+int main() {
+    printf("%s", "happy birthday");
+    return 0;
+}
